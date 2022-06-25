28b6369eda35f0ed7cacfebd3e49387f561efc24
Rachel <rachel.gray7448@gmail.com>
2022-06-24 23:38:32 -0400

    added component files/folders, started nav component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#project">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            Contact Me
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
