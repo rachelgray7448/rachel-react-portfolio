34793b6476416c621d8487d22ecef278ff1b5a55
Rachel <rachel.gray7448@gmail.com>
2022-06-28 19:57:59 -0400

    got navigation styling functional

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,13 +7,12 @@ function Navigation({ currentPage, handlePageChange }) {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Rachel Gray</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <NavDropdown title="Projects" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">
                                 Action
                             </NavDropdown.Item>
@@ -23,11 +22,8 @@ function Navigation({ currentPage, handlePageChange }) {
                             <NavDropdown.Item href="#action/3.3">
                                 Something
                             </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
