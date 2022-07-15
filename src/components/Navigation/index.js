92863671477a1b7c766709f5be1d59d2a99dd062
Rachel <rachel.gray7448@gmail.com>
2022-07-14 20:54:07 -0400

    added DDAA to nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,42 +1,91 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import { Container, NavDropdown, Nav } from "react-bootstrap"
+import { Container, NavDropdown, Nav } from "react-bootstrap";
 
 function Navigation({ currentPage, handlePageChange }) {
-
     return (
         <Navbar className="navbar" expand="lg">
             <Container className="navbar-internal">
-                <Navbar.Brand className="super-internal" href="#home">Rachel Gray</Navbar.Brand>
+                <Navbar.Brand className="super-internal" href="#home">
+                    Rachel Gray
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
+                <Navbar.Collapse
+                    className="super-internal"
+                    id="basic-navbar-nav"
+                >
                     <Nav className="me-auto">
-                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("About")}>About</Nav.Link>
-                        <NavDropdown className="super-internal" title="Projects" id="basic-nav-dropdown">
-                            <NavDropdown.Item target="blank" href="https://limitless-journey-42984.herokuapp.com/">
+                        <Nav.Link
+                            className="super-internal"
+                            onClick={() => handlePageChange("About")}
+                        >
+                            About
+                        </Nav.Link>
+                        <NavDropdown
+                            className="super-internal"
+                            title="Projects"
+                            id="basic-nav-dropdown"
+                        >
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://limitless-journey-42984.herokuapp.com/"
+                            >
                                 The B Word
                             </NavDropdown.Item>
-                            <NavDropdown.Item target="blank" href="https://rachelgray7448.github.io/run-buddy/">
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://rachelgray7448.github.io/run-buddy/"
+                            >
                                 Run Buddy
                             </NavDropdown.Item>
-                            <NavDropdown.Item target="blank" href="https://rachelgray7448.github.io/taskinator/">
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://rachelgray7448.github.io/taskinator/"
+                            >
                                 Taskinator
                             </NavDropdown.Item>
-                            <NavDropdown.Item target="blank" href="https://rachelgray7448.github.io/food-festival/">
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://rachelgray7448.github.io/food-festival/"
+                            >
                                 Food Festival
                             </NavDropdown.Item>
-                            <NavDropdown.Item target="blank" href="https://rachelgray7448.github.io/swanson-quote-quiz/">
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://rachelgray7448.github.io/swanson-quote-quiz/"
+                            >
                                 Swanson Quote Quiz
                             </NavDropdown.Item>
-                            <NavDropdown.Item target="blank" href="https://rachelgray7448.github.io/weather-dashboard/">
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://rachelgray7448.github.io/weather-dashboard/"
+                            >
                                 Weather Dashboard
                             </NavDropdown.Item>
-                            <NavDropdown.Item onClick={()=>handlePageChange("Project")}>
+                            <NavDropdown.Item
+                                target="blank"
+                                href="https://delusionaldinos.herokuapp.com/"
+                            >
+                                The DDAA
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => handlePageChange("Project")}
+                            >
                                 View All
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Resume")}>Experience</Nav.Link>
-                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Contact")}>Contact</Nav.Link>
+                        <Nav.Link
+                            className="super-internal"
+                            onClick={() => handlePageChange("Resume")}
+                        >
+                            Experience
+                        </Nav.Link>
+                        <Nav.Link
+                            className="super-internal"
+                            onClick={() => handlePageChange("Contact")}
+                        >
+                            Contact
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
